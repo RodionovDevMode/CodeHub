1c29ade18e0c294ddef0f3ce37dcfb7572182c2e
Andrei Rodionov <rodionovdevmode@MacBook-Air-Andrei.local>
2025-08-14 16:25:09 +0300

    Был добавлен react,react-dom и типы для него, так же был рассширен конфиг и добавлен sass-loader.Так же создан тестовый компонент классический счётчик для проверки работы пакетов

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,15 +1,11 @@
 import path from 'path'
 import webpack from 'webpack'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import { buildPlugins } from './config/build/buildPlugins'
-import { buildLoaders } from './config/build/buildLoaders'
-import { buildResolvers } from './config/build/buildResolvers'
 import { buildWebpackConfig } from './config/build/buildWebpackConfig'
 import { BuildEnv, BuildPaths } from './config/build/types/config'
 
 export default (env: BuildEnv) => {
 	const paths: BuildPaths = {
-		entry: path.resolve(__dirname, 'src', 'index.ts'),
+		entry: path.resolve(__dirname, 'src', 'index.tsx'),
 		build: path.resolve(__dirname, 'build'),
 		html: path.resolve(__dirname, 'public', 'index.html'),
 	}
