89983a5317b4254e473f036466fd9d2777ad31cd
Andrei Rodionov <rodionovdevmode@MacBook-Air-Andrei.local>
2025-08-19 12:20:40 +0300

    Add react-router-dom and types

---
+++ build/src_components_pages_MainPage_MainPage_tsx.eb222ced9a756b2cafe6.js
@@ -0,0 +1,24 @@
+"use strict";
+(self["webpackChunkrodionovproject"] = self["webpackChunkrodionovproject"] || []).push([["src_components_pages_MainPage_MainPage_tsx"],{
+
+/***/ "./src/components/pages/MainPage/MainPage.tsx":
+/*!****************************************************!*\
+  !*** ./src/components/pages/MainPage/MainPage.tsx ***!
+  \****************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ MainPage)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+function MainPage() {
+    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("h1", { children: "Main Page" }) }));
+}
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX2NvbXBvbmVudHNfcGFnZXNfTWFpblBhZ2VfTWFpblBhZ2VfdHN4LmViMjIyY2VkOWE3NTZiMmNhZmU2LmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7O0FBQWdEO0FBQ2pDO0FBQ2YsWUFBWSxzREFBSSxVQUFVLFVBQVUsc0RBQUksU0FBUyx1QkFBdUIsR0FBRztBQUMzRSIsInNvdXJjZXMiOlsid2VicGFjazovL3JvZGlvbm92cHJvamVjdC8uL3NyYy9jb21wb25lbnRzL3BhZ2VzL01haW5QYWdlL01haW5QYWdlLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBqc3ggYXMgX2pzeCB9IGZyb20gXCJyZWFjdC9qc3gtcnVudGltZVwiO1xuZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gTWFpblBhZ2UoKSB7XG4gICAgcmV0dXJuIChfanN4KFwiZGl2XCIsIHsgY2hpbGRyZW46IF9qc3goXCJoMVwiLCB7IGNoaWxkcmVuOiBcIk1haW4gUGFnZVwiIH0pIH0pKTtcbn1cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
