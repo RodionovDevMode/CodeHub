2e08612f6adf17a08a6cb5fc5890f5d67a7e3e1b
Andrei Rodionov <rodionovdevmode@MacBook-Air-Andrei.local>
2025-08-20 18:24:36 +0300

    Add global styles and context for theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './index.scss'
-import { Link } from 'react-router-dom'
+import { Suspense } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
 import { AboutPageLazy } from './components/pages/AboutPage/AboutPage.lazy'
 import { MainPagLazy } from './components/pages/MainPage/MainPage.lazy'
-import { Suspense } from 'react'
+import './styles/index.scss'
+import { useTheme } from './theme/useTheme'
 
 export default function App() {
+	const { theme, toggleTheme } = useTheme()
+
 	return (
-		<div className='app'>
+		<div className={`app ${theme}`}>
+			<button onClick={toggleTheme}>Change Theme</button>
 			<Link to={'/'}>Главная страница</Link>
 			<Link to={'/about'}>О сайте</Link>
 			<Suspense fallback={<div>Загрузка...</div>}>
