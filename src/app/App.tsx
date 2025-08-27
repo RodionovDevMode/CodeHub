3f92ad677672952c3e62542866e5cf643d36cbde
Andrei Rodionov <rodionovdevmode@MacBook-Air-Andrei.local>
2025-08-27 12:41:47 +0300

    add Navbar.tsx, AppLink.tsx and styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,17 @@
-import { Suspense } from 'react'
-import { Link, Route, Routes } from 'react-router-dom'
 import './styles/index.scss'
 import { classNames } from '../shared/lib/classNames/classNames'
-import { AboutPage } from 'pages/AboutPage'
-import { MainPage } from 'pages/MainPage'
 import { useTheme } from 'app/providers/ThemeProvider'
 import { AppRouter } from './providers/ThemeProvider/router'
+import { Navbar } from 'widgets/Navbar'
 
 function App() {
 	const { theme, toggleTheme } = useTheme()
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
+			<Navbar />
 			<button onClick={toggleTheme}>Change Theme</button>
-			<Link to={'/'} style={{ paddingLeft: '15px', paddingRight: '15px' }}>
-				Главная страница
-			</Link>
-			<Link to={'/about'}>О сайте</Link>
+
 			<AppRouter />
 		</div>
 	)
